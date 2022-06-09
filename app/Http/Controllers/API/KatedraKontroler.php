bc767e8b51cb2a7ad786fec46619937b0ca9bbf3
Marija273 <maka.totovic@gmail.com>
2022-06-10 00:37:10 +0200

    5. zahtev - KatedraKontroler i ProfesorKontroler, šest api ruta

---
+++ app/Http/Controllers/API/KatedraKontroler.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Katedra;
+use Illuminate\Http\Request;
+
+class KatedraKontroler extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Katedra  $katedra
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Katedra $katedra)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Katedra  $katedra
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Katedra $katedra)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Katedra  $katedra
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Katedra $katedra)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Katedra  $katedra
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Katedra $katedra)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\API\KatedraKontroler;
+use App\Http\Controllers\API\ProfesorKontroler;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('katedra', [KatedraKontroler::class, 'index']);
+Route::get('katedra/{katedra}', [KatedraKontroler::class, 'show']);
+Route::put('katedra/{katedra}', [KatedraKontroler::class, 'update']);
+Route::get('profesor', [ProfesorKontroler::class, 'index']);
+Route::get('profesor/{profesor}', [ProfesorKontroler::class, 'show']);
+Route::delete('profesor/{profesor}', [ProfesorKontroler::class, 'destroy']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
